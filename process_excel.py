7fc2d9ce965e71518b3090adb246236abc7eb90f
Antoine-Mtx <antoine.mortureux@gmail.com>
2023-04-24 22:10:49 +0200

    finalised process_excel script

--- process_excel.py
+++ process_excel.py
@@ -1,48 +1,65 @@
 import pandas as pd
 import json
-import re
 
-def process_sheet(sheet):
-    df = sheet.fillna(method='ffill', axis=0)
-    categories = []
+def concat_cells(group, col_index):
+    non_empty_cells = group[group.columns[col_index]].dropna()
+    deduplicated_cells = []
+    for cell in non_empty_cells:
+        if cell not in deduplicated_cells:
+            deduplicated_cells.append(cell)
+    return '\n'.join(deduplicated_cells)
 
-    for _, group in df.groupby('Unnamed: 0'):
-        category_dict = {
-            'Catégorie': group['Unnamed: 0'].iloc[0],
-            'Questions': [],
-            'Score': group['Score'].iloc[0]
-        }
+def process_sheet(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name=sheet_name, engine='openpyxl', header=3)
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    df.iloc[:, 0] = df.iloc[:, 0].fillna(method='ffill')
+    df.iloc[:, 2] = df.iloc[:, 2].fillna(method='ffill')
+    progress_step_col_index = df.columns.get_loc("Démarche pour progresser") if "Démarche pour progresser" in df.columns else df.columns.get_loc("Vos idées pour progresser")
+    df.iloc[:, progress_step_col_index] = df.iloc[:, progress_step_col_index].fillna(method='ffill')
+    df = df.rename(columns={'Items': 'Catégorie'})
+    grouped = df.groupby('Catégorie')
+    output = []
 
-        for _, row in group.iterrows():
-            if not pd.isna(row['Questionnements']):
-                question_dict = {
-                    'Question': row['Questionnements'],
-                    'Réponse': int(row['Score.1']),
-                    '2': row['2 points'],
-                    '1': row['1 point'],
-                    '0': row['0 point'],
-                    'Commentaires': row['Commentaires/Justification (exemples concrets)'],
-                }
-                category_dict['Questions'].append(question_dict)
+    for name, group in grouped:
+        category_dict = {'Catégorie': name}
+        questions = []
+        for index, row in group.iterrows():
+            if pd.isnull(row['Questionnements']):
+                continue
+            question_dict = {
+                'Question': row['Questionnements'],
+                'Réponse': int(row['Score.1']),
+                '2': row['2 points'],
+                '1': row['1 point'],
+                '0': row['0 point'],
+            }
+            if 'Commentaires/Justification (exemples concrets)' in row:
+                question_dict['Commentaire'] = row['Commentaires/Justification (exemples concrets)']
+            questions.append(question_dict)
+        category_dict['Questions'] = questions
+        category_dict['Score'] = group['Score'].iloc[0]
+        category_group = group.loc[group['Catégorie'] == name]
+        category_dict['Démarche pour progresser'] = concat_cells(category_group, progress_step_col_index)
+        output.append(category_dict)
 
-        categories.append(category_dict)
+    return output
 
-    return categories
+file_path = 'J0_Grille_Acces_Editions.xlsx'
+sheets = ['Axe Compétences', 'Axe Réactivité', 'Axe Numérique']
+data = {}
 
-def main():
-    excel_file = 'J0_Grille_Acces_Editions.xlsx'
-    company_name = re.sub(r'JO_Grille_(.*).xlsx', r'\1', excel_file).replace('_', ' ')
-    xls = pd.read_excel(excel_file, sheet_name=['Axe Compétences', 'Axe Réactivité', 'Axe Numérique'])
+# Extraire le nom de l'entreprise à partir du nom du fichier
+file_name = file_path.split('.')[0]  # Enlever l'extension '.xlsx'
+name_parts = file_name.split('_')[2:]  # Ignorer les deux premières parties ('J0' et 'Grille')
+entreprise_name = ' '.join(name_parts)  # Joindre les parties restantes avec un espace
 
-    final_output = {
-        'Entreprise': company_name,
-        'Axe Compétences': process_sheet(xls['Axe Compétences']),
-        'Axe Réactivité': process_sheet(xls['Axe Réactivité']),
-        'Axe Numérique': process_sheet(xls['Axe Numérique']),
-    }
+data['Entreprise'] = entreprise_name
 
-    with open('output.json', 'w') as outfile:
-        json.dump(final_output, outfile, ensure_ascii=False, indent=4)
 
-if __name__ == '__main__':
-    main()
+for sheet_name in sheets:
+    data[sheet_name] = process_sheet(file_path, sheet_name)
+
+json_data = json.dumps(data, ensure_ascii=False, indent=4)
+
+with open('output.json', 'w', encoding='utf-8') as json_file:
+    json_file.write(json_data)
